c408b045c8327175cea75f5d445886904b1eb9fa
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2023-11-04 13:47:38 +1300

    PATCH: fixing resizing

--- src/ResizeAssetsRunner.php
+++ src/ResizeAssetsRunner.php
@@ -12,6 +12,8 @@ class ResizeAssetsRunner
     protected static $useGd = false;
     protected static $patterns_to_skip = [];
     protected static $max_file_size_in_mb = 2;
+    protected static $default_quality = 0.77;
+    protected static $large_size_default_quality = 0.67;
 
     public static function set_imagick_as_converter()
     {
@@ -27,7 +29,17 @@ class ResizeAssetsRunner
 
     public static function set_max_file_size_in_mb(?int $max_file_size_in_mb = 2)
     {
-        self::$max_file_size_in_mb = $max_file_size_in_mb = $max_file_size_in_mb;
+        self::$max_file_size_in_mb = $max_file_size_in_mb;
+    }
+
+    public static function set_default_quality(?float $default_quality = 0.77)
+    {
+        self::$default_quality =  $default_quality;
+    }
+
+    public static function set_large_size_default_quality(?float $large_size_default_quality = 0.67)
+    {
+        self::$large_size_default_quality = $large_size_default_quality;
     }
 
     public static function set_gd_or_imagick_as_converter()
@@ -56,24 +68,40 @@ class ResizeAssetsRunner
                         continue 2;
                     }
                 }
-                self::run_one($file->getPathname(), $maxWidth, $maxHeight, $dryRun);
+                self::run_one(
+                    $file->getPathname(),
+                    $maxWidth,
+                    $maxHeight,
+                    self::$default_quality,
+                    $dryRun
+                );
                 $sizeCheck = self::isFileSizeGreaterThan($file->getPathname());
                 if($sizeCheck) {
                     list($width, $height) = getimagesize($file->getPathname());
                     $ratio = $width / $height;
-                    self::run_one($file->getPathname(), round($width - (1 * $sizeCheck * $ratio)), round($height - (1 * $sizeCheck * $ratio)), $dryRun, true);
+                    self::run_one(
+                        $file->getPathname(),
+                        round($width - (1  * $ratio)),
+                        round($height - (1 * $ratio)),
+                        self::$large_size_default_quality, // image quality is low to resize file size
+                        $dryRun,
+                        true // force resize!
+                    );
                     echo 'ERROR! ' . $file . ' is still ' . $sizeCheck . '% too big!' . PHP_EOL;
                 }
             }
         }
     }
 
-    public static function run_one(string $img, int $maxWidth, int $maxHeight, ?bool $dryRun = true, ?bool $force = false)
+    public static function run_one(string $path, int $maxWidth, int $maxHeight, ?float $quality = 0.77, ?bool $dryRun = true, ?bool $force = false)
     {
-        list($width, $height) = getimagesize($img);
+        if($quality > 1 && $quality < 0.3) {
+            user_error("Error: Quality should be between 0.3 and 1.0.\n");
+        }
+        list($width, $height) = getimagesize($path);
         if ($width <= $maxWidth && $height <= $maxHeight && !$force) {
             if($dryRun) {
-                echo "-- skipping $img ({$width}x{$height})" . PHP_EOL;
+                echo "-- skipping $path ({$width}x{$height})" . PHP_EOL;
             }
             return;
         }
@@ -83,52 +111,107 @@ class ResizeAssetsRunner
         $newHeight = (int) round($height * $ratio);
 
         if ($dryRun) {
-            echo "Dry run: Would resize $img ({$width}x{$height}) to ($newWidth x $newHeight)" . PHP_EOL;
+            echo "Dry run: Would resize $path ({$width}x{$height}) to ($newWidth x $newHeight)" . PHP_EOL;
             return;
         }
 
-        echo "Resizing $img" . PHP_EOL;
+        echo "Resizing $path" . PHP_EOL;
 
 
         if(self::$useImagick) {
-            /** @var \Imagick $Image */
-            $image = new \Imagick($img);
-            $image->resizeImage($newWidth, $newHeight, Imagick::FILTER_LANCZOS, 1);
-            $image->writeImage($img);
-            $image->clear();
-            $image->destroy();
+            user_error('You will have to manually run imagick.');
+
+            // KEEP!
+            // KEEP!
+            // KEEP!
+            // KEEP!
+            // KEEP!
+            // /** @var \Imagick $Image */
+            // $image = new \Imagick($img);
+            // $image->resizeImage($newWidth, $newHeight, Imagick::FILTER_LANCZOS, 1);
+            // $outputFormat = self::get_output_path($path);
+            // try {
+
+            //     // Set the compression quality for JPEG
+            //     if ($outputFormat === 'jpeg' || $outputFormat === 'jpg') {
+            //         $jpgQuality = round($quality * 99);
+            //         $image->setImageCompression(Imagick::COMPRESSION_JPEG);
+            //         $image->setImageCompressionQuality($jpgQuality);
+            //     }
+
+            //     // Set the format to PNG for output if needed
+            //     if ($outputFormat === 'png') {
+            //         $pngQuality = round($quality * 9);
+            //         $image->setImageFormat('png');
+            //         $image->setImageCompressionQuality($pngQuality);
+            //     }
+
+            //     // For GIF, you may want to handle animations
+            //     if ($outputFormat === 'gif') {
+            //         $image = $image->coalesceImages();
+            //         foreach ($image as $frame) {
+            //             $frame->resizeImage($newWidth, $newHeight, Imagick::FILTER_LANCZOS, 1);
+            //             $frame->setImageCompressionQuality($quality);
+            //         }
+            //         $imagick = $image->deconstructImages();
+            //         $imagick->setImageFormat('gif');
+            //     }
+
+            //     // Write the image back to the file
+            //     $image->writeImages($path, true);
+
+            //     // Clear memory
+            //     $imagick->clear();
+            //     $imagick->destroy();
+
+            //     return true;
+            // } catch (ImagickException $e) {
+            //     error_log("An error occurred while compressing the image: " . $e->getMessage());
+            //     return false;
+            // }
+
+            // KEEP!
+            // KEEP!
+            // KEEP!
+            // KEEP!
+            // KEEP!
+
+
+            echo "$path NOT DONE.";
         } elseif(self::$useGd) {
 
             // Create a new true color image
             $newImage = imagecreatetruecolor($newWidth, $newHeight);
 
             // Load the image
-            switch (strtolower(pathinfo($img, PATHINFO_EXTENSION))) {
+            switch (self::get_output_path($path)) {
                 case 'jpg':
                 case 'jpeg':
-                    $sourceImage = imagecreatefromjpeg($img);
+                    $sourceImage = imagecreatefromjpeg($path);
                     break;
                 case 'png':
-                    $sourceImage = imagecreatefrompng($img);
+                    $sourceImage = imagecreatefrompng($path);
                     break;
                 case 'gif':
-                    $sourceImage = imagecreatefromgif($img);
+                    $sourceImage = imagecreatefromgif($path);
                     break;
             }
             if($sourceImage) {
                 imagecopyresampled($newImage, $sourceImage, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
 
                 // Save the image
-                switch (strtolower(pathinfo($img, PATHINFO_EXTENSION))) {
+                switch (strtolower(pathinfo($path, PATHINFO_EXTENSION))) {
                     case 'jpg':
                     case 'jpeg':
-                        imagejpeg($newImage, $img);
+                        $jpgQuality = round($quality * 99);
+                        imagejpeg($newImage, $path, $jpgQuality);
                         break;
                     case 'png':
-                        imagepng($newImage, $img);
+                        $pngQuality = round($quality * 9);
+                        imagepng($newImage, $path, $pngQuality);
                         break;
                     case 'gif':
-                        imagegif($newImage, $img);
+                        imagegif($newImage, $path);
                         break;
                 }
 
@@ -148,10 +231,11 @@ class ResizeAssetsRunner
         if(self::$useImagick || self::$useGd) {
             return;
         }
-        if (extension_loaded('imagick')) {
-            self::$useImagick = true;
-        } elseif (extension_loaded('gd')) {
+        // preferred...
+        if (extension_loaded('gd')) {
             self::$useGd = true;
+        } elseif (extension_loaded('imagick')) {
+            self::$useImagick = true;
         } else {
             exit("Error: Neither Imagick nor GD is installed.\n");
         }
@@ -164,5 +248,11 @@ class ResizeAssetsRunner
             return round(($fileSize - $maxSize) / $maxSize * 100);
         }
         return null;
+    }
+
+
+    protected static function get_output_path(string $path): string
+    {
+        return strtolower(pathinfo($path, PATHINFO_EXTENSION));
     }
 }
